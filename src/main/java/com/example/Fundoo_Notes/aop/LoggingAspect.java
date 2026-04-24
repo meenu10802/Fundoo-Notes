2df0389e5083a53981a987e67343ad6f981ee9a2
meenu10802 <meenu10802@gmail.com>
2026-04-24 23:48:46 +0530

    [Meenaskhi]: UC11: Centralized AOP Logging

--- src/main/java/com/example/Fundoo_Notes/aop/LoggingAspect.java
+++ src/main/java/com/example/Fundoo_Notes/aop/LoggingAspect.java
@@ -15,17 +15,23 @@ public class LoggingAspect {
 
     @Around("execution(* com.example.Fundoo_Notes.service..*(..)) || execution(* com.example.Fundoo_Notes.controller..*(..))")
     public Object logAndTrackExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+
         long start = System.currentTimeMillis();
         String method = joinPoint.getSignature().toShortString();
-        log.info("Entering {}", method);
+
+        log.info("➡️ Entering {} with args {}", method, joinPoint.getArgs());
 
         try {
             Object result = joinPoint.proceed();
+
             long end = System.currentTimeMillis();
-            log.info("Completed {} in {} ms", method, (end - start));
+            log.info("✅ Completed {} in {} ms", method, (end - start));
+
             return result;
+
         } catch (Exception ex) {
-            log.error("Failed in {} with error: {}", method, ex.getMessage());
+
+            log.error("❌ Failed in {}", method, ex);
             throw ex;
         }
     }
